3521b90e947b2649f45ffac6ee5c95b689c4e65f
Vadim-Zheliazkov <vadim@glassok.ua>
2023-06-11 20:24:52 +0300

    it-done-11-06-24

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,72 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// <li class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </li>;
+
+// CREATE LIST AND ADD TO UL=======================
+// ================================================
+
+const listPhoto = document.querySelector(".gallery");
+listPhoto.addEventListener("click", makeBigPhoto);
+const listGalleryItems = iterateArrayGalleryItems(galleryItems);
+console.log(listGalleryItems);
+function iterateArrayGalleryItems(list) {
+  return list
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+listPhoto.innerHTML = listGalleryItems;
+
+const accessToLinkFromA = document.querySelector(".gallery__link");
+
+function makeBigPhoto(evt) {
+  evt.preventDefault();
+  accessToLinkFromA.toggleAttribute("href");
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  // CONNECT MODAL TO LIST=======================
+  // ================================================
+
+  const instance = basicLightbox.create(`
+ <img src ="${evt.target.dataset.source}" width='800' height='600'>
+`);
+
+  instance.show();
+
+  listPhoto.addEventListener("keydown", closeModalBtnEsc);
+
+  // CLOSE MODAL WITH HELP OF ESC=======================
+  // ================================================
+
+  function closeModalBtnEsc(evt) {
+    if (evt.key === "Escape") {
+      instance.close();
+      listPhoto.removeEventListener("keydown", closeModalBtnEsc);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+  </a>
+</li> */
+
+const listPhoto = document.querySelector(".gallery");
+
+const listGalleryItems = iterateArrayGalleryItems(galleryItems);
+console.log(listGalleryItems);
+function iterateArrayGalleryItems(list) {
+  return list
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+listPhoto.innerHTML = listGalleryItems;
+
+const accessToLinkFromA = document.querySelector(".gallery__link");
+
+listPhoto.addEventListener("click", handleFunctionForPhotoList);
+
+function handleFunctionForPhotoList(evt) {
+  evt.preventDefault();
+  accessToLinkFromA.toggleAttribute("href");
+
+  console.log(evt.target);
+  console.log(evt.target.src);
+
+  // ================================================ // CREATE NEW MARKUP=======================
+
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
